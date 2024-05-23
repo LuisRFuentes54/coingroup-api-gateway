402223b62d54a72e370c315b5ea6543643403eae
LuisRFuentes54 <luisrf0540@gmail.com>
2024-05-23 19:06:05 -0400

    feat: change the initial endpoint to receive POST request

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Post()
+  getHello(@Body() data: any): string {
+    console.log(data);
     return this.appService.getHello();
   }
 }
