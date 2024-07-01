f81fc152c41a061f4a1732e73d9cb47015504ecb
LuisRFuentes54 <luisrf0540@gmail.com>
2024-07-01 13:30:21 -0400

    feat: remittance module's initial structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { RemittanceModule } from './remittance/remittance.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [RemittanceModule]
 })
 export class AppModule {}
