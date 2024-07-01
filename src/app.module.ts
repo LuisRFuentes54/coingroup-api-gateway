a1cd47d792f9202c1caf37830c757ca564662d60
LuisRFuentes54 <luisrf0540@gmail.com>
2024-07-01 15:57:05 -0400

    feat: add dependencies for db connection and enviroment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
 import { RemittanceModule } from './remittance/remittance.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [RemittanceModule]
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }), 
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PG_DB_HOST,
+      port: parseInt(process.env.PG_DB_PORT),
+      username: process.env.PG_DB_USER,
+      password: process.env.PG_DB_PASSWORD,
+      database: process.env.PG_DB_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+    RemittanceModule
+  ]
 })
 export class AppModule {}
